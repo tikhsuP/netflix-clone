f8f62ed56f4cf01ecfe1e08ee31f702ba058bee8
tikhsuP <pushkit20@gmail.com>
2020-10-30 12:52:08 +0530

    Integrate API and display thumbnails

---
+++ netflix/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "f4e5d156f0caf6436266f0be709b9cfe";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
